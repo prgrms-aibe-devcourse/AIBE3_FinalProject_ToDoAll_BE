8fc96c9a15fe4af72a5a4cbcc69a3cbd9cf9d950
seungin <tmddls91@inha.edu>
2025-11-02 19:34:31 +0900

    [feat] User 도메인 관련 컨트롤러 초기 구성

---
+++ src/main/java/com/server/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.server.user.controller;
+
+import com.server.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+}

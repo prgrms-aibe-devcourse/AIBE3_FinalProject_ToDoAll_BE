e4c69346f754ade71dd850da9833ce8c6e950ec6
seungin <tmddls91@inha.edu>
2025-11-02 18:58:08 +0900

    [feat] 전역 관리용 ApplicationException 추가

---
+++ src/main/java/com/server/global/exception/ApplicationException.java
@@ -0,0 +1,14 @@
+package com.server.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ApplicationException extends RuntimeException {
+
+    private final ErrorCase errorCase;
+
+    public ApplicationException(ErrorCase errorCase) {
+        super(errorCase.getMessage());
+        this.errorCase = errorCase;
+    }
+}

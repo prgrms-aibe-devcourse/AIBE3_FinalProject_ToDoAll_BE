d8e52f8801cbf78d552538fc7d1c25651b5f1a16
seungin <tmddls91@inha.edu>
2025-11-04 00:09:28 +0900

    [feat] 채용공고 관련 레포지토리 초기 구성

---
+++ src/main/java/com/server/jd/repository/JobDescriptionRepository.java
@@ -0,0 +1,10 @@
+package com.server.jd.repository;
+
+import com.server.jd.domain.JobDescription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface JobDescriptionRepository extends JpaRepository<JobDescription, Long> {
+    List<JobDescription> findAllByStatus(String status);
+}

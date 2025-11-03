f987109ca5e63a9e7d516906be963aa8e7de1185
seungin <tmddls91@inha.edu>
2025-11-04 01:18:43 +0900

    [feat] 웹소켓 관련 파일 초기 구성

---
+++ src/main/java/com/server/interview/websocket/InterviewStompConfig.java
@@ -0,0 +1,18 @@
+package com.server.interview.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+/**
+ * STOMP WebSocket 설정
+ * - WebSocket 메시지 브로커 설정
+ * - 클라이언트와 서버 간의 실시간 통신을 위한 엔드포인트 및 브로커 구성
+ */
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class InterviewStompConfig implements WebSocketMessageBrokerConfigurer {
+}
+

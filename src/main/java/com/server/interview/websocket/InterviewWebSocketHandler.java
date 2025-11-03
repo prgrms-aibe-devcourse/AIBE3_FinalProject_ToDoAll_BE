e0d09dc7a5c07f56808dd75706318882fe40778f
seungin <tmddls91@inha.edu>
2025-11-04 01:18:43 +0900

    [feat] 웹소켓 관련 파일 초기 구성

---
+++ src/main/java/com/server/interview/websocket/InterviewWebSocketHandler.java
@@ -0,0 +1,13 @@
+package com.server.interview.websocket;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ * WebSocket 핸들러
+ * - 텍스트 기반 실시간 메시지를 처리합니다.
+ * - 예시: 면접 관련 실시간 채팅, 질문 공유 등
+ * - WebSocket 세션 연결, 해제, 메시지 수신 등을 처리
+ */
+public class InterviewWebSocketHandler extends TextWebSocketHandler {
+    // TODO: 연결/해제/메시지 처리 구현 예정
+}

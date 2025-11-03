2d8a8ee7eade8c2ee47d5545f2bfbf259d36a41b
seungin <tmddls91@inha.edu>
2025-11-04 00:52:51 +0900

    [feat] 추천 관련 엘라스틱 컴포넌트 초기 구성

---
+++ src/main/java/com/server/match/elastic/MatchingElasticClient.java
@@ -0,0 +1,9 @@
+package com.server.match.elastic;
+
+import org.springframework.stereotype.Component;
+
+// ElasticSearch에서 JD-이력서 매칭 검색을 담당하는 컴포넌트
+@Component
+public class MatchingElasticClient {
+    // TODO: ES Hybrid Search 로직 구현
+}

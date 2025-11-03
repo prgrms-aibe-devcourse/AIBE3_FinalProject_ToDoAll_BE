a56e212cdfe275f85fac6b8a46f42d0380267a09
seungin <tmddls91@inha.edu>
2025-11-04 00:55:32 +0900

    [feat] 추천 관련 컨트롤러 초기 구성

---
+++ src/main/java/com/server/match/controller/MatchController.java
@@ -0,0 +1,12 @@
+package com.server.match.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/match")
+public class MatchController {
+    // 매칭 추천 결과 조회 API 등 구현
+}

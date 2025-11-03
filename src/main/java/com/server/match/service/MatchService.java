3ec5d53ec07741d642377978869b26f8c7b75800
seungin <tmddls91@inha.edu>
2025-11-04 00:51:53 +0900

    [feat] 추천 관련 서비스 초기 구성

---
+++ src/main/java/com/server/match/service/MatchService.java
@@ -0,0 +1,11 @@
+package com.server.match.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// JD와 이력서 매칭 비즈니스 로직
+@Service
+@RequiredArgsConstructor
+public class MatchService {
+    // JD와 이력서 매칭 로직은 추후 ElasticSearch 연동 후 구현
+}

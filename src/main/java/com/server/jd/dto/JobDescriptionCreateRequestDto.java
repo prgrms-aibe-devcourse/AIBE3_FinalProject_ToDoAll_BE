7b63c5e13b5112ac35ec641caa6644cdfd36f91f
seungin <tmddls91@inha.edu>
2025-11-04 00:12:57 +0900

    [feat] 채용공고 관련 request,response Dto 초기 구성

---
+++ src/main/java/com/server/jd/dto/JobDescriptionCreateRequestDto.java
@@ -0,0 +1,20 @@
+package com.server.jd.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record JobDescriptionCreateRequestDto(
+        // 필드는 필요하다면 본인이 직접 수정 및 추가
+        String title,
+        String department,
+        String region,
+        String workType,
+        String experience,
+        String education,
+        String salary,
+        String description,
+        LocalDate deadline,
+        List<String> requiredSkills,
+        List<String> preferredSkills,
+        String welfare
+) {}

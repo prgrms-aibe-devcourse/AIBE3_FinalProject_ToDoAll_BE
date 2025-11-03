4e83695f817e01f11a1131ab5ecb3c5bd8d47d90
seungin <tmddls91@inha.edu>
2025-11-04 00:33:04 +0900

    [feat] 이력서 관련 ErrorCase 초기 구성

---
+++ src/main/java/com/server/resume/exception/ResumeErrorCase.java
@@ -0,0 +1,31 @@
+package com.server.resume.exception;
+
+import com.server.global.exception.ErrorCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum ResumeErrorCase implements ErrorCase {
+
+    RESUME_NOT_FOUND(HttpStatus.NOT_FOUND, 7001, "해당 이력서를 찾을 수 없습니다."),
+    RESUME_OWNER_MISMATCH(HttpStatus.FORBIDDEN, 7002, "이력서 소유자가 아닙니다.");
+
+    private final HttpStatus httpStatus;
+    private final Integer errorCode;
+    private final String message;
+
+    @Override
+    public Integer getHttpStatusCode() {
+        return httpStatus.value();
+    }
+
+    @Override
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}

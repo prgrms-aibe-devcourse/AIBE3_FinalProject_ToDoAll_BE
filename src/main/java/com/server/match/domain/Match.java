ec4e6a150275aac871c153bd3fb68aae6fe39463
seungin <tmddls91@inha.edu>
2025-11-07 15:42:39 +0900

    [feat] JD-이력서 매칭 관련 엔티티 기본 구현

---
+++ src/main/java/com/server/match/domain/Match.java
@@ -0,0 +1,41 @@
+package com.server.match.domain;
+
+import com.server.global.entity.BaseEntity;
+import com.server.jd.domain.JobDescription;
+import com.server.resume.domain.Resume;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Match extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // JD 연관관계 (ManyToOne)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "jd_id", nullable = false)
+    private JobDescription jobDescription;
+
+    // Resume 연관관계 (ManyToOne)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "resume_id", nullable = false)
+    private Resume resume;
+
+    private LocalDateTime appliedAt; // 지원 일시 
+
+    private Float matchScore; // 매칭 점수
+
+    @Lob
+    private String recommendationReason; // 추천 사유
+
+    @Enumerated(EnumType.STRING)
+    private MatchStatus status;
+}

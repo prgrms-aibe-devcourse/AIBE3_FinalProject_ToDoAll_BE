b784a62332eef4a90c922a8887865df9cba16e42
seungin <tmddls91@inha.edu>
2025-11-07 14:52:08 +0900

    [feat] 웹소켓 예시 메시지 핸들러 추가

--- src/main/java/com/server/interview/controller/InterviewController.java
+++ src/main/java/com/server/interview/controller/InterviewController.java
@@ -2,6 +2,8 @@ package com.server.interview.controller;
 
 import com.server.interview.service.InterviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,4 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class InterviewController {
     private final InterviewService interviewService;
+
+
+    // 예시 메시지 핸들러 (실제 구현할때 지워주세요.)
+    @MessageMapping("/interview/message")
+    @SendTo("/topic/interview/room/{roomId}")
+    public String handleInterviewMessage(String message) {
+        return "[면접 메시지] " + message;
+    }
 }

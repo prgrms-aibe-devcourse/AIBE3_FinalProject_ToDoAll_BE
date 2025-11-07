2160a8e65c4f2c495856b76c1430cd6fd8fad523
seungin <tmddls91@inha.edu>
2025-11-07 15:59:57 +0900

    [feat] 면접 노트 관련 엔티티 초기 구현

---
+++ src/main/java/com/server/interview/domain/InterviewNote.java
@@ -0,0 +1,32 @@
+package com.server.interview.domain;
+
+import com.server.global.entity.BaseEntity;
+import com.server.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class InterviewNote extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 면접 ID (FK)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interview_id", nullable = false)
+    private Interview interview;
+
+    // 작성자 (유저)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    // 면접 노트 내용
+    @Column(columnDefinition = "TEXT")
+    private String content;
+}

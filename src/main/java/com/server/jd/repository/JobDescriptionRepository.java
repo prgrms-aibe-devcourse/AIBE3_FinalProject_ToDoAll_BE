247431c0206d98b4b75c52c2caa2b0eb9821cdcd
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-11-10 15:24:10 +0900

    [feat] Repository 쿼리문 작성

--- src/main/java/com/server/jd/repository/JobDescriptionRepository.java
+++ src/main/java/com/server/jd/repository/JobDescriptionRepository.java
@@ -2,9 +2,18 @@ package com.server.jd.repository;
 
 import com.server.jd.domain.JobDescription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface JobDescriptionRepository extends JpaRepository<JobDescription, Long> {
     List<JobDescription> findAllByStatus(String status);
+    @Query("""
+            select jd.id as jobId, s
+            from JobDescription jd
+            join jd.requiredSkills s
+            where jd.id in :ids
+            """)
+    List<Object[]> findRequiredSkillsByJobIds(Collection<Long> ids);
 }

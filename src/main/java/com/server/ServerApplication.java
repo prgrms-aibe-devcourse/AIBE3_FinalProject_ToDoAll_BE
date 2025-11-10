3082ea7ab67efe2afc14ba1c68aa39325927b560
seungin <tmddls91@inha.edu>
2025-11-10 15:45:17 +0900

    [refactor] flyway 관련 로직 임시 제거 (DB 안정화 후 다시 복구 예정)

--- src/main/java/com/server/ServerApplication.java
+++ src/main/java/com/server/ServerApplication.java
@@ -2,9 +2,9 @@ package com.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
-@SpringBootApplication(exclude = { FlywayAutoConfiguration.class })
+
+@SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {

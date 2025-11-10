030d9a936a7e06745a41ca0e485b9733fd203691
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-11-10 15:35:33 +0900

    [feat] 컨트롤러 매핑 구현

--- src/main/java/com/server/jd/controller/JobDescriptionController.java
+++ src/main/java/com/server/jd/controller/JobDescriptionController.java
@@ -1,8 +1,13 @@
 package com.server.jd.controller;
 
 
+import com.server.jd.dto.JobDescriptionListResponseDto;
 import com.server.jd.service.JobDescriptionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,4 +15,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/jd")
 public class JobDescriptionController {
     private final JobDescriptionService jobService;
+
+    @GetMapping
+    public Page<JobDescriptionListResponseDto> list(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10")int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
+        return jobService.getList(pageable, 5);
+    }
 }

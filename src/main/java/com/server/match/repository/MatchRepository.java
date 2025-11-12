1e05210e3aa86fc8719d2d37bb6eb8da947a31a6
seungin <tmddls91@inha.edu>
2025-11-12 16:20:38 +0900

    [feat] 매칭 관련 레포지토리 구성

---
+++ src/main/java/com/server/match/repository/MatchRepository.java
@@ -0,0 +1,11 @@
+package com.server.match.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.server.match.domain.Match;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Long> {
+
+    boolean existsByJobDescription_IdAndResume_Id(Long jdId, Long resumeId);
+}

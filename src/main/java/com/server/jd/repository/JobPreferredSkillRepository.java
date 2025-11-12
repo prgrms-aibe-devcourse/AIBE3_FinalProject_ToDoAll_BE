ae40d68706e8ac6d5be86353c340a6264e64c0dc
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-11-12 15:35:41 +0900

    [refactor] 공고 기술 Object -> Projection으로 수정하여 타입 안전성 높임

--- src/main/java/com/server/jd/repository/JobPreferredSkillRepository.java
+++ src/main/java/com/server/jd/repository/JobPreferredSkillRepository.java
@@ -1,6 +1,7 @@
 package com.server.jd.repository;
 
 import com.server.jd.domain.JobPreferredSkill;
+import com.server.jd.repository.projection.SkillByJobProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,5 +14,12 @@ public interface JobPreferredSkillRepository extends JpaRepository<JobPreferredS
         from JobPreferredSkill jps
         where jps.job.id in :ids
     """)
-    List<Object[]> findPreferredSkillsByJobIds(Collection<Long> ids);
+    List<SkillByJobProjection> findPreferredSkillsByJobIds(Collection<Long> ids);
+
+    @Query("""
+        select jps.skill.name
+        from JobPreferredSkill jps
+        where jps.job.id = :id
+    """)
+    List<String> findPreferredSkillNamesByJobId(Long id);
 }

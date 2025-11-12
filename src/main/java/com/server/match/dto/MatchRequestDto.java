0fbfabfae880eb20a2f6d44857dcfda6a0e6fac9
seungin <tmddls91@inha.edu>
2025-11-12 17:44:15 +0900

    [refactor] 유효성 검증 메시지 추가

--- src/main/java/com/server/match/dto/MatchRequestDto.java
+++ src/main/java/com/server/match/dto/MatchRequestDto.java
@@ -3,6 +3,8 @@ package com.server.match.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record MatchRequestDto(
-        @NotNull Long jdId,        // 채용공고 ID
-        @NotNull Long resumeId     // 이력서 ID
+        @NotNull(message = "채용공고 ID는 필수입니다.")
+        Long jdId,        // 채용공고 ID
+        @NotNull(message = "이력서 ID는 필수입니다.")
+        Long resumeId     // 이력서 ID
 ) {}

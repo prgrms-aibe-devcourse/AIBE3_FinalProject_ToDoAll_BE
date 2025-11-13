3ed345508a817d01567c4e5ef40b45f16ce0f9aa
Unoguna <kimjungho.kr@gmail.com>
2025-11-13 17:47:19 +0900

    [chore] 스웨거 예시 설정

--- src/main/java/com/server/interview/dto/InterviewCreateResponseDto.java
+++ src/main/java/com/server/interview/dto/InterviewCreateResponseDto.java
@@ -1,6 +1,9 @@
 package com.server.interview.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record InterviewCreateResponseDto (
-    Long interviewId
+        @Schema(description = "면접 ID", example = "1")
+        Long interviewId
 ){
 }

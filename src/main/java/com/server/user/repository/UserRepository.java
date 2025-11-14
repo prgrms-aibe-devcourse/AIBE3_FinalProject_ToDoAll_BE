db626c6f914dbe17bddc25cb11d85d21893c4138
jiyoon-00 <pulip2517@gmail.com>
2025-11-14 11:21:23 +0900

    [feat] 이메일 중복 검사 메서드 정의

--- src/main/java/com/server/user/repository/UserRepository.java
+++ src/main/java/com/server/user/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.server.user.repository;
 
 import com.server.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }

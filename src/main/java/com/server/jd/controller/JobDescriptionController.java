63c4368473d889ff040c63d6c62102fbe3c75ac9
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-11-14 10:38:46 +0900

    [feat] 컨트롤러 메서드 구현

--- src/main/java/com/server/jd/controller/JobDescriptionController.java
+++ src/main/java/com/server/jd/controller/JobDescriptionController.java
@@ -5,7 +5,9 @@ import com.server.global.response.CommonResponse;
 import com.server.jd.dto.JobDescriptionCreateRequestDto;
 import com.server.jd.dto.JobDescriptionDetailResponseDto;
 import com.server.jd.dto.JobDescriptionListResponseDto;
+import com.server.jd.dto.SkillResponseDto;
 import com.server.jd.service.JobDescriptionService;
+import com.server.jd.service.SkillQueryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,12 +18,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/jd")
 public class JobDescriptionController {
     private final JobDescriptionService jobService;
+    private final SkillQueryService skillQueryService;
 
     @GetMapping
     public ResponseEntity<CommonResponse<Page<JobDescriptionListResponseDto>>> list(
@@ -49,5 +53,8 @@ public class JobDescriptionController {
     }
 
     @GetMapping("/skills")
-    public ResponseEntity<CommonResponse<>>
+    public ResponseEntity<CommonResponse<List<SkillResponseDto>>> getSkills() {
+        List<SkillResponseDto> Skills = skillQueryService.getSkills();
+        return ResponseEntity.ok(CommonResponse.success(Skills));
+    }
 }

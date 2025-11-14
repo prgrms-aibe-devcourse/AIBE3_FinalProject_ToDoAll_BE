739630fbb8d201f942cd27130ecfb761c9159a57
seungin <tmddls91@inha.edu>
2025-11-15 02:35:42 +0900

    [fix] CI 워크플로우 수정 및 yml 설정파일 분리

--- src/test/java/com/server/match/repository/MatchQueryRepositoryImplTest.java
+++ src/test/java/com/server/match/repository/MatchQueryRepositoryImplTest.java
@@ -19,9 +19,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -31,6 +34,7 @@ import static com.server.user.domain.TestFixtures.createUser;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @Transactional
 class MatchQueryRepositoryImplTest {
 

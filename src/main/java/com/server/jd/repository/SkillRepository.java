7efc58191970c95a44ad4d3fab99eee9212c776b
SWWWin <bgpd1258@naver.com>
2025-11-14 11:26:26 +0900

    [fix] 충돌 문제 해결

--- src/main/java/com/server/jd/repository/SkillRepository.java
+++ src/main/java/com/server/jd/repository/SkillRepository.java
@@ -4,6 +4,8 @@ import com.server.jd.domain.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface SkillRepository extends JpaRepository<Skill, Long> {

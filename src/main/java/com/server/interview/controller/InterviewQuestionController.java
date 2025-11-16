7a1ede145d178632397bb935c1d7deb66843de9d
Unoguna <kimjungho.kr@gmail.com>
2025-11-16 22:44:10 +0900

    [refactor] 스웨거 예외처리 강화

--- src/main/java/com/server/interview/controller/InterviewQuestionController.java
+++ src/main/java/com/server/interview/controller/InterviewQuestionController.java
@@ -4,6 +4,8 @@ import com.server.global.response.CommonResponse;
 import com.server.interview.dto.InterviewQuestionUpdateRequestDto;
 import com.server.interview.service.InterviewQuestionService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +21,12 @@ public class InterviewQuestionController {
 
     @PutMapping
     @Operation(summary = "인터뷰 질문 업데이트", description = "인터뷰 질문을 삭제, 수정, 등록 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "인터뷰 업데이트 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 면접입니다."),
+            @ApiResponse(responseCode = "403", description = "질문을 등록할 권한이 없습니다."),
+            @ApiResponse(responseCode = "400", description = "요청 형식이 올바르지 않습니다.")
+    })
     public CommonResponse<String> updateQuestions(
             @PathVariable Long interviewId,
             @RequestBody @Valid InterviewQuestionUpdateRequestDto request
@@ -26,4 +34,10 @@ public class InterviewQuestionController {
         interviewQuestionService.updateQuestions(interviewId, request);
         return CommonResponse.success("면접 질문이 성공적으로 업데이트되었습니다.");
     }
+//
+//    @GetMapping
+//    @Operation(summary = "인터뷰 질문 조회", description = "인터뷰에 해당하는 질문을 전체조회 합니다.")
+//    public CommonResponse<List<InterviewQuestion>> getQuestions(
+//
+//    )
 }

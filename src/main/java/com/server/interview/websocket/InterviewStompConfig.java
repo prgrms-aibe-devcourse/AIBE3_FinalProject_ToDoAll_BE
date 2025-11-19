46438e21c2afb04e20d102c5e10049117e290f21
SWWWin <bgpd1258@naver.com>
2025-11-19 09:41:01 +0900

    [feat] ResumeResponseDto에 지원일자, 메모 추가 및 STOMP WebSocket 설정 추가

--- src/main/java/com/server/interview/websocket/InterviewStompConfig.java
+++ src/main/java/com/server/interview/websocket/InterviewStompConfig.java
@@ -1,7 +1,10 @@
 package com.server.interview.websocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
@@ -11,8 +14,22 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
  * - 클라이언트와 서버 간의 실시간 통신을 위한 엔드포인트 및 브로커 구성
  */
 
+@EnableWebSocket
 @Configuration
 @EnableWebSocketMessageBroker
 public class InterviewStompConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/interview")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
 }
 

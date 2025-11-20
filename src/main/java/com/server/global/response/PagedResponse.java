a7f1fab2f1946e9b1647b199899dd3c55b7cc4f9
seungin <tmddls91@inha.edu>
2025-11-20 17:22:42 +0900

    [feat] Page용 응답 구조 구성

---
+++ src/main/java/com/server/global/response/PagedResponse.java
@@ -0,0 +1,16 @@
+package com.server.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class PagedResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+}

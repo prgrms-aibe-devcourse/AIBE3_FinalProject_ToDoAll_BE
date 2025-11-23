a3ae5d1902ea21476ff700f8db90a4303733651d
AEU <aeuplayground@gmail.com>
2025-11-23 12:51:54 +0900

    [chore] compose-cd 수정

--- wait-for-mysql.sh
+++ wait-for-mysql.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 host="db"
 port=3306
 user="root"
@@ -8,28 +7,33 @@ password="${MYSQL_ROOT_PASSWORD}"
 database="${MYSQL_DATABASE}"
 max_retries=30
 sleep_seconds=2
+wait_for_db=${WAIT_FOR_DB}
 
-echo "Waiting for MySQL ($host:$port) to accept connections..."
+if [ "$wait_for_db" = "true" ]; then
+  echo "Waiting for MySQL ($host:$port) to accept connections..."
 
-until nc -z "$host" "$port"; do
-  echo "❌ MySQL port not open yet..."
-  sleep $sleep_seconds
-done
-echo "✅ Port open. Checking full readiness..."
+  until nc -z "$host" "$port"; do
+    echo "❌ MySQL port not open yet..."
+    sleep $sleep_seconds
+  done
+  echo "✅ Port open. Checking full readiness..."
 
-counter=0
-until mysql -h"$host" -P"$port" -u"$user" -p"$password" -e "SELECT 1;" "$database" > /dev/null 2>&1; do
-  counter=$((counter+1))
-  echo "❌ MySQL not ready yet... ($counter/$max_retries)"
-  if [ $counter -ge $max_retries ]; then
-    echo "💥 MySQL did not become ready after $((max_retries * sleep_seconds)) seconds. Exiting."
-    exit 1
-  fi
-  sleep $sleep_seconds
-done
+  counter=0
+  until mysql -h"$host" -P"$port" -u"$user" -p"$password" -e "SELECT 1;" "$database" > /dev/null 2>&1; do
+    counter=$((counter+1))
+    echo "❌ MySQL not ready yet... ($counter/$max_retries)"
+    if [ $counter -ge $max_retries ]; then
+      echo "💥 MySQL did not become ready after $((max_retries * sleep_seconds)) seconds. Exiting."
+      exit 1
+    fi
+    sleep $sleep_seconds
+  done
 
-echo "✅ MySQL fully ready. Waiting 5 more seconds for internal stabilization..."
-sleep 10
+  echo "✅ MySQL fully ready. Waiting 5 more seconds for internal stabilization..."
+  sleep 10
+else
+  echo "⚡ Skipping DB wait (WAIT_FOR_DB == false)"
+fi
 
 echo "🚀 Starting Spring Boot app..."
 

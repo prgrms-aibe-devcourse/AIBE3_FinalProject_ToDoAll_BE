d5ec2e86cc04462e8ea546cb2d7d36e74908ee9f
SWWWin <bgpd1258@naver.com>
2025-11-25 17:04:50 +0900

    [fix] build test 통과를 위한 config 분리

--- src/main/java/com/server/interview/websocket/TestWebSocketConfig.java
+++ src/main/java/com/server/interview/websocket/TestWebSocketConfig.java
@@ -1,10 +1,7 @@
 package com.server.interview.websocket;
 
-import com.server.interview.websocket.security.WebSocketJwtInterceptor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -12,11 +9,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-@RequiredArgsConstructor
-@Profile("!test")
-public class InterviewStompConfig implements WebSocketMessageBrokerConfigurer {
-
-    private final WebSocketJwtInterceptor webSocketJwtInterceptor;
+@Profile("test")
+public class TestWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -29,10 +23,5 @@ public class InterviewStompConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic", "/queue");
         registry.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(webSocketJwtInterceptor);
     }
 }

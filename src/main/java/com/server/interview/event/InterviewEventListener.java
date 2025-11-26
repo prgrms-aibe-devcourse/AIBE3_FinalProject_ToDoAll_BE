d8c8bca906ccaec68233ee1b4d26c703bc789494
Unoguna <kimjungho.kr@gmail.com>
2025-11-26 09:35:37 +0900

    [refactor] 알림 저장 실패시 SSE 알림 보내지지 않고 에러 로그 출력되게 변경

--- src/main/java/com/server/interview/event/InterviewEventListener.java
+++ src/main/java/com/server/interview/event/InterviewEventListener.java
@@ -3,11 +3,12 @@ package com.server.interview.event;
 import com.server.interview.repository.InterviewParticipantRepository;
 import com.server.notification.domain.Notification;
 import com.server.notification.domain.NotificationType;
-import com.server.notification.payload.InterviewPayload;
 import com.server.notification.dto.NotificationRequestDto;
+import com.server.notification.payload.InterviewPayload;
 import com.server.notification.service.NotificationService;
 import com.server.notification.service.SseService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class InterviewEventListener {
 
     private final NotificationService notificationService;
@@ -30,21 +32,25 @@ public class InterviewEventListener {
         List<Long> userIds = participantRepository.findUserIdsByInterviewId(event.interviewId());
 
         for (Long userId : userIds) {
-            // Notification 생성
-            Notification notification = notificationService.saveNotification(
-                    new NotificationRequestDto(
-                            userId,
-                            NotificationType.INTERVIEW,
-                            "면접 생성 알림",
-                            "새로운 면접이 생성되었습니다!",
-                            new InterviewPayload(event.interviewId()),
-                            true,
-                            null
-                    )
-            );
+            try {
+                Notification notification = notificationService.saveNotification(
+                        new NotificationRequestDto(
+                                userId,
+                                NotificationType.INTERVIEW,
+                                "면접 생성 알림",
+                                "새로운 면접이 생성되었습니다!",
+                                new InterviewPayload(event.interviewId()),
+                                true,
+                                null
+                        )
+                );
 
-            // SSE 알림 전송
-            sseService.sendNotification(notification);
+                // 저장 성공한 경우에만 SSE 발송
+                sseService.sendNotification(notification);
+
+            } catch (Exception e) {
+                log.error("Notification 저장 실패 - userId: {}", userId, e);
+            }
         }
     }
 }

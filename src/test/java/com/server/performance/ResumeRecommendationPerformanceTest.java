4250f319c028cda95b57dd47aa1ba4b15c0ba56a
seungin <tmddls91@inha.edu>
2025-11-26 14:37:24 +0900

    [test] 성능테스트 로직은 CI 빌드에서 제외

--- src/test/java/com/server/performance/ResumeRecommendationPerformanceTest.java
+++ src/test/java/com/server/performance/ResumeRecommendationPerformanceTest.java
@@ -2,6 +2,7 @@ package com.server.performance;
 
 import com.server.support.TestEnvLoader;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@EnabledIfEnvironmentVariable(named = "CI", matches = "false")
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")

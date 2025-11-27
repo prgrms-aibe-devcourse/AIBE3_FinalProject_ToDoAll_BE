5c2cfa58af270b52b8fb07a5319d1fba7bf00711
AEU <aeuplayground@gmail.com>
2025-11-28 00:24:52 +0900

    [feat] 에러코드 수정

--- src/main/java/com/server/s3/exception/S3ErrorCase.java
+++ src/main/java/com/server/s3/exception/S3ErrorCase.java
@@ -6,9 +6,9 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum S3ErrorCase implements ErrorCase {
-    FILENAME_NOT_FOUND(HttpStatus.BAD_REQUEST, 10001, "업로드한 파일에 파일명이 없습니다."),
-    FILE_UPLOAD_FAIL(HttpStatus.BAD_REQUEST, 10001, "S3에 파일을 업로드 하는 중 문제가 발생했습니다."),
-    INVALID_FILE_KEY(HttpStatus.BAD_REQUEST, 10001, "잘못된 파일키입니다.");
+    FILENAME_NOT_FOUND(HttpStatus.BAD_REQUEST, 12001, "업로드한 파일에 파일명이 없습니다."),
+    FILE_UPLOAD_FAIL(HttpStatus.BAD_REQUEST, 12002, "S3에 파일을 업로드 하는 중 문제가 발생했습니다."),
+    INVALID_FILE_KEY(HttpStatus.BAD_REQUEST, 12003, "잘못된 파일키입니다.");
 
     private final HttpStatus httpStatus;
     private final Integer errorCode;

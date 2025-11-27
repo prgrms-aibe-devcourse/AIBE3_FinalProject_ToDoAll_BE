e17e1907ee2244472443312ffdcf15cf0ddec455
seungin <tmddls91@inha.edu>
2025-11-27 14:35:32 +0900

    [refactor] 매칭 확정 상태 관련 enum 추가

--- src/main/java/com/server/match/domain/MatchStatus.java
+++ src/main/java/com/server/match/domain/MatchStatus.java
@@ -3,6 +3,7 @@ package com.server.match.domain;
 public enum MatchStatus {
     APPLIED,        // 지원됨
     RECOMMENDED,    // 추천됨
+    CONFIRMED,
     BOOKMARK,       // 북마크됨
     HOLD,           // 보류됨
     REJECTED        // 거절됨

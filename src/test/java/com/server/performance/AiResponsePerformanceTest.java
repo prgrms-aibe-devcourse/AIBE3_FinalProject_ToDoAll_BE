e942d67af06013088a227e0c38393773cc292a16
seungin <tmddls91@inha.edu>
2025-11-27 11:46:19 +0900

    [fix] 성능테스트는 CI 빌드 제외

--- src/test/java/com/server/performance/AiResponsePerformanceTest.java
+++ src/test/java/com/server/performance/AiResponsePerformanceTest.java
@@ -5,11 +5,13 @@ import com.server.match.cache.RedisRecommendationCacheService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @Slf4j
+@EnabledIfEnvironmentVariable(named = "CI", matches = "false")
 @SpringBootTest
 @ActiveProfiles("test")
 public class AiResponsePerformanceTest {

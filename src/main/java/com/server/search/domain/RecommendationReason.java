45c43137c31ebbd6c0f13efa7c63e36b76e6a72f
seungin <tmddls91@inha.edu>
2025-12-02 15:38:53 +0900

    [feat] 추천 사유 생성 관련 엔티티 정의 (복합 키 클래스 포함)

---
+++ src/main/java/com/server/search/domain/RecommendationReason.java
@@ -0,0 +1,22 @@
+package com.server.search.domain;
+
+import com.server.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "recommendation_reasons")
+@IdClass(RecommendationKey.class)
+public class RecommendationReason extends BaseEntity {
+    @Id
+    private Long jdId;
+
+    @Id
+    private Long resumeId;
+
+    @Lob
+    private String reason;
+}

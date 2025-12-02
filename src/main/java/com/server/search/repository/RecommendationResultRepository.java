c6c6451b41836cf63cde49a43154f05f73b63884
seungin <tmddls91@inha.edu>
2025-12-02 17:24:19 +0900

    [feat] 채용공고에 따른 이력서 전체 캐시 삭제 관련 쿼리 메서드 추가

--- src/main/java/com/server/search/repository/RecommendationResultRepository.java
+++ src/main/java/com/server/search/repository/RecommendationResultRepository.java
@@ -3,6 +3,8 @@ package com.server.search.repository;
 import com.server.search.domain.RecommendationResult;
 import com.server.search.domain.RecommendationKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.List;
@@ -10,4 +12,7 @@ import java.util.List;
 public interface RecommendationResultRepository extends JpaRepository<RecommendationResult, RecommendationKey> {
     Optional<RecommendationResult> findByJdIdAndResumeId(Long jdId, Long resumeId);
     List<RecommendationResult> findAllByJdId(Long jdId);
+
+    @Query("SELECT r.resumeId FROM RecommendationResult r WHERE r.jdId = :jdId")
+    List<Long> findResumeIdsByJdId(@Param("jdId") Long jdId);
 }

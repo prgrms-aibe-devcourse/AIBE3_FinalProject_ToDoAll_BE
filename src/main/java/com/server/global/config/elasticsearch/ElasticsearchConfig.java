cc08edd826aa8471b5c331c093c2f80054741bb2
AEU <aeuplayground@gmail.com>
2025-12-03 11:43:26 +0900

    [chore] ElasticsearchConfig 빈 생성 제한

--- src/main/java/com/server/global/config/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/com/server/global/config/elasticsearch/ElasticsearchConfig.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!ci")
+@Profile("!cd")
 public class ElasticsearchConfig {
 
     @Value("${spring.elasticsearch.uris}")

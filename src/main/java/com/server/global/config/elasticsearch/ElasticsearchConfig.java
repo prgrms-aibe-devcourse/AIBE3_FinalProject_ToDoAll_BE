0ef84e88c86404d0512955575eb9c272ce98e823
AEU <aeuplayground@gmail.com>
2025-12-03 12:07:49 +0900

    [chore] cd 프로필에 elasticsearch 필드 및 기타 라인 추가

--- src/main/java/com/server/global/config/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/com/server/global/config/elasticsearch/ElasticsearchConfig.java
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!cd")
 public class ElasticsearchConfig {
 
     @Value("${spring.elasticsearch.uris}")

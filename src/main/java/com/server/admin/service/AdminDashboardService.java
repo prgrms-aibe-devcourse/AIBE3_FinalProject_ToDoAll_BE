7b1a4013d16ba22635eb622045d183fbbea335ea
seungin <tmddls91@inha.edu>
2025-12-05 02:16:54 +0900

    [refactor] 어드민 대시보드 서비스에 최근 유저,채용공고 관련 로직 추가

--- src/main/java/com/server/admin/service/AdminDashboardService.java
+++ src/main/java/com/server/admin/service/AdminDashboardService.java
@@ -1,6 +1,8 @@
 package com.server.admin.service;
 
 import com.server.admin.dto.AdminDashboardSummaryDto;
+import com.server.admin.dto.RecentJdDto;
+import com.server.admin.dto.RecentUserDto;
 import com.server.interview.repository.InterviewRepository;
 import com.server.jd.repository.JobDescriptionRepository;
 import com.server.resume.repository.ResumeRepository;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -37,5 +40,19 @@ public class AdminDashboardService {
                 totalResumes,
                 totalInterviews, todayInterviews
         );
+    }
+
+    public List<RecentUserDto> getRecentUsers() {
+        return userRepository.findTop5ByDeletedAtIsNullOrderByCreatedAtDesc()
+                .stream()
+                .map(RecentUserDto::from)
+                .toList();
+    }
+
+    public List<RecentJdDto> getRecentJds() {
+        return jdRepository.findTop5ByOrderByCreatedAtDesc()
+                .stream()
+                .map(RecentJdDto::from)
+                .toList();
     }
 }

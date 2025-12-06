e19e7da696b14f072d6a80bb10979faf85ad427a
seungin <tmddls91@inha.edu>
2025-12-07 01:28:54 +0900

    [refactor] 이력서 형식 관련 ErrorCase 추가

--- src/main/java/com/server/resume/exception/ResumeErrorCase.java
+++ src/main/java/com/server/resume/exception/ResumeErrorCase.java
@@ -11,9 +11,9 @@ public enum ResumeErrorCase implements ErrorCase {
     RESUME_OWNER_MISMATCH(HttpStatus.FORBIDDEN, 4042, "이력서 소유자가 아닙니다."),
     JD_NOT_FOUND(HttpStatus.NOT_FOUND, 4043, "해당 공고를 찾을 수 없습니다."),
     SKILL_NOT_FOUND(HttpStatus.NOT_FOUND, 4044, "해당 스킬을 찾을 수 없습니다."),
-    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, 4001, "이력서 입력값이 유효하지 않습니다."),
-    INVALID_STATUS(HttpStatus.BAD_REQUEST, 4002, "유효하지 않은 상태 변경입니다."),
-    ;
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, 4045, "이력서 입력값이 유효하지 않습니다."),
+    INVALID_STATUS(HttpStatus.BAD_REQUEST, 4046, "유효하지 않은 상태 변경입니다."),
+    INVALID_DATA(HttpStatus.BAD_REQUEST, 4047, "이력서 데이터 형식이 올바르지 않습니다."),;
 
     private final HttpStatus httpStatus;
     private final Integer errorCode;

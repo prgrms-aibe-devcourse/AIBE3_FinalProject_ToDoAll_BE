c1d4c4f6d2fd3854761bd5b7dc073465cdae17e2
Unoguna <kimjungho.kr@gmail.com>
2025-12-14 23:48:19 +0900

    [refactor] 이력서 이미지 url 확인을 위해 S3 불러오는 기능 잠시 주석처리

--- src/main/java/com/server/interview/repository/InterviewRepositoryImpl.java
+++ src/main/java/com/server/interview/repository/InterviewRepositoryImpl.java
@@ -84,14 +84,16 @@ public class InterviewRepositoryImpl implements InterviewRepositoryCustom {
             String finalUrl = null;
             String dbFileKey = base.candidateAvatar();
 
-            if (dbFileKey != null && dbFileKey.startsWith("resume")) {
-                try {
-                    finalUrl = presignedUrlProvider.createPresignedGetUrl(dbFileKey);
-                } catch (Exception e) {
-                    log.warn("Failed to create presigned url. fileKey={}", dbFileKey, e);
-                    finalUrl = null; // 명시적으로
-                }
-            }
+//            if (dbFileKey != null && dbFileKey.startsWith("resume")) {
+//                try {
+//                    finalUrl = presignedUrlProvider.createPresignedGetUrl(dbFileKey);
+//                } catch (Exception e) {
+//                    log.warn("Failed to create presigned url. fileKey={}", dbFileKey, e);
+//                    finalUrl = null; // 명시적으로
+//                }
+//            }
+
+            finalUrl = base.candidateAvatar();
 
             interviews.set(idx, new InterviewSummaryDto(
                     base.interviewId(),

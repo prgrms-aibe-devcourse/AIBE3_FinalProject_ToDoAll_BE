65239ea20cfb4c4f92c692dc35dd7ebd8c2aae7f
Unoguna <kimjungho.kr@gmail.com>
2026-01-18 12:15:53 +0900

    [refactor] ApplicationEventPublisher 하나로 통일

--- src/main/java/com/server/interview/service/InterviewService.java
+++ src/main/java/com/server/interview/service/InterviewService.java
@@ -135,7 +135,7 @@ public class InterviewService {
 
 
         //MCP 면접 질문 자동 생성 및 저장 로직
-        applicationEventPublisher.publishEvent(
+        eventPublisher.publishEvent(
                 new InterviewCreatedAiEvent(
                         interview.getId()
                 )
